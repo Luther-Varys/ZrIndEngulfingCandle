9e86212fad970fd513de33ce8d7670560ccb0bf4
Carlo Braminixxx <braminicarlo@live.com>
2024-04-20 23:04:02 +0100

    added standard deviation for candle delta high and low

--- ZrIndEngulfingCandle/ZrIndEngulfingCandle.cs
+++ ZrIndEngulfingCandle/ZrIndEngulfingCandle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using cAlgo.API;
@@ -26,7 +27,7 @@ namespace cAlgo
             Print(Message);
         }
 
-
+        int standardDeviationSampleNumber = 10;
         double tollerancePercent = 0;
         double bodyPercentVsWick = 200;
         public override void Calculate(int index)
@@ -38,6 +39,14 @@ namespace cAlgo
 
             var barPreLast = bars.TakeLast(3).First();
             var barLast = bars.TakeLast(2).First();
+
+            if (bars.Count > standardDeviationSampleNumber +1)
+            {
+                var candlesStandardDeviation = GetCandlesStandardDeviation(standardDeviationSampleNumber);
+                Debug.WriteLine($"ZR: candlesStandardDeviation {candlesStandardDeviation.ToString()}");
+            }
+
+
 
             //case pre candle is red and last candle is green
             if (barPreLast.Open > barPreLast.Close && barLast.Open < barLast.Close)
@@ -77,5 +86,61 @@ namespace cAlgo
 
             Result[index - 1] = 0;
         }
+
+
+        private double GetCandlesStandardDeviation(int countCandles)
+        {
+            {
+                var deltaHighLowOfCandles = GetDeltaHighLowOfCandles(countCandles);
+
+
+                var resp = SampleStandardDeviation(deltaHighLowOfCandles);
+
+                return resp;
+            }
+
+            List<double> GetDeltaHighLowOfCandles(int numberOfCandles)
+            {
+                var resp = new List<double>();
+
+                var bars = MarketData.GetBars(this.TimeFrame);
+
+                if (bars.Count == 0)
+                    throw new Exception("ZR: there are 0 candles, high low delta cannot be evaluated ");
+
+                if ((numberOfCandles + 1 < bars.Count) == false)
+                {
+                    return resp;
+                }
+
+                for (int i = 1; i <= numberOfCandles; i++)
+                {
+                    var barTemp = bars.TakeLast(i + 1).First();
+                    resp.Add(Math.Abs(barTemp.High - barTemp.Low));
+                }
+
+                return resp;
+            }
+
+            double StandardDeviation(List<double> numberSet, double divisor)
+            {
+                double mean = numberSet.Average();
+                return Math.Sqrt(numberSet.Sum(x => Math.Pow(x - mean, 2)) / divisor);
+            }
+
+            double PopulationStandardDeviation(List<double> numberSet)
+            {
+                var resp = StandardDeviation(numberSet, numberSet.Count);
+                return resp;
+            }
+
+            double SampleStandardDeviation(List<double> numberSet)
+            {
+                var resp = StandardDeviation(numberSet, numberSet.Count - 1);
+                return resp;
+            }
+        }
+
+
     }
 }
